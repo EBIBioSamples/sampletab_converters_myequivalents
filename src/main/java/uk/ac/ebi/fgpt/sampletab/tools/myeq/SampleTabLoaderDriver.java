0f9da49dbdc72da4b2f57c258231aa0f76b7e7ff
Adam Faulconbridge <afaulconbridge@googlemail.com>
2014-05-16 14:47:44 +0100

    Cant override annotations of arguments in parent class, using required options instead.

--- src/main/java/uk/ac/ebi/fgpt/sampletab/tools/myeq/SampleTabLoaderDriver.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/tools/myeq/SampleTabLoaderDriver.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.List;
 
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,30 +16,16 @@ import uk.ac.ebi.fgpt.sampletab.AbstractInfileDriver;
 public class SampleTabLoaderDriver extends AbstractInfileDriver<SampleTabLoaderTask> {
 
     
-    @Argument(required=true, index=0, metaVar="USERNAME", usage = "username for myEquivalents")
+    @Option(required=true, name = "-u", aliases={"--username"}, metaVar="USERNAME", usage = "username for myEquivalents")
     protected String username;
 
-    @Argument(required=true, index=1, metaVar="PASSWORD", usage = "password for myEquivalents")
+    @Option(required=true, name = "-p", aliases={"--password"}, metaVar="PASSWORD", usage = "password for myEquivalents")
     protected String password;
-
-
-    //set a high index since the one multi-value argument MUST be the last argument
-    @Argument(required=true, index=2, metaVar="INPUT", usage = "input filenames or globs")
-    protected List<String> inputFilenames;
     
     private EntityMappingManager emMgr = null;
     
     // logging
     private Logger log = LoggerFactory.getLogger(getClass());
-
-    @Override
-    /**
-     * Ensure this class uses the variable specified here with different annotation
-     * rather than the variable in the superclass with other annotations
-     */
-    protected List<String> getInputFilenames() {
-        return inputFilenames;
-    }
 
     public static void main(String[] args) {
         new SampleTabLoaderDriver().doMain(args);
