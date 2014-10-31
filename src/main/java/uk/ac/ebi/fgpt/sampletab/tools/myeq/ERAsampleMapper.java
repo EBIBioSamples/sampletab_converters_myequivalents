7152cfa8708b34070c7f530552a1b48863fac46e
Adam Faulconbridge <afaulconbridge@googlemail.com>
2014-10-31 11:42:51 +0000

    Refactored and condensed ENA mapper

--- src/main/java/uk/ac/ebi/fgpt/sampletab/tools/myeq/ERAsampleMapper.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/tools/myeq/ERAsampleMapper.java
@@ -9,10 +9,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,136 +28,130 @@ import com.jolbox.bonecp.BoneCPDataSource;
  */
 public class ERAsampleMapper {
 
-	private Logger log = LoggerFactory.getLogger(getClass());
-	private BoneCPDataSource ds = null;
-	private String username = null;
-	private String password = null;
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private BoneCPDataSource ds = null;
+    private String username = null;
+    private String password = null;
 
-	private static ERAsampleMapper instance = null;
+    private static ERAsampleMapper instance = null;
 
-	/**
-	 * Private constructor, use getInstance() method to ensure singleton pattern
-	 * is followed
-	 */
-	private ERAsampleMapper() {
+    /**
+     * Private constructor, use getInstance() method to ensure singleton pattern
+     * is followed
+     */
+    private ERAsampleMapper() {
 
-	}
+    }
 
-	public synchronized static ERAsampleMapper getInstance() {
-		if (instance == null) {
-			instance = new ERAsampleMapper();
-		}
-		return instance;
-	}
+    public synchronized static ERAsampleMapper getInstance() {
+        if (instance == null) {
+            instance = new ERAsampleMapper();
+        }
+        return instance;
+    }
 
-	protected synchronized BoneCPDataSource getDataSource()
-			throws ClassNotFoundException {
-		if (ds == null) {
-			synchronized (getClass()) {
-				// load defaults
-				Properties properties = new Properties();
-				try {
-					InputStream is = getClass().getResourceAsStream(
-							"/era-pro.properties");
-					properties.load(is);
-				} catch (IOException e) {
-					log.error("Unable to read resource era-pro.properties", e);
-				}
-				String hostname = properties.getProperty("hostname");
-				Integer port = new Integer(properties.getProperty("port"));
-				String database = properties.getProperty("database");
-				username = properties.getProperty("username");
-				password = properties.getProperty("password");
+    protected synchronized BoneCPDataSource getDataSource() throws ClassNotFoundException {
+        if (ds == null) {
+            synchronized (getClass()) {
+                // load defaults
+                Properties properties = new Properties();
+                try {
+                    InputStream is = getClass().getResourceAsStream("/era-pro.properties");
+                    properties.load(is);
+                } catch (IOException e) {
+                    log.error("Unable to read resource era-pro.properties", e);
+                }
+                String hostname = properties.getProperty("hostname");
+                Integer port = new Integer(properties.getProperty("port"));
+                String database = properties.getProperty("database");
+                username = properties.getProperty("username");
+                password = properties.getProperty("password");
 
-				try {
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-				} catch (ClassNotFoundException e) {
-					log.error("Unable to find oracle.jdbc.driver.OracleDriver",
-							e);
-					throw e;
-				}
-				String jdbc = "jdbc:oracle:thin:@" + hostname + ":" + port
-						+ ":" + database;
-				log.trace("JDBC URL = " + jdbc);
-				log.trace("USER = " + username);
-				log.trace("PW = " + password);
+                try {
+                    Class.forName("oracle.jdbc.driver.OracleDriver");
+                } catch (ClassNotFoundException e) {
+                    log.error("Unable to find oracle.jdbc.driver.OracleDriver", e);
+                    throw e;
+                }
+                String jdbc = "jdbc:oracle:thin:@" + hostname + ":" + port + ":" + database;
+                log.trace("JDBC URL = " + jdbc);
+                log.trace("USER = " + username);
+                log.trace("PW = " + password);
 
-				ds = new BoneCPDataSource();
-				ds.setJdbcUrl(jdbc);
-				ds.setUsername(username);
-				ds.setPassword(password);
+                ds = new BoneCPDataSource();
+                ds.setJdbcUrl(jdbc);
+                ds.setUsername(username);
+                ds.setPassword(password);
 
-				ds.setPartitionCount(1);
-				ds.setMaxConnectionsPerPartition(10);
-				ds.setAcquireIncrement(2);
-			}
-		}
-		return ds;
-	}
+                ds.setPartitionCount(1);
+                ds.setMaxConnectionsPerPartition(10);
+                ds.setAcquireIncrement(2);
+            }
+        }
+        return ds;
+    }
 
-	public Map<String, String> getPublicSampleIds(Date minDate, Date maxDate) {
-		PreparedStatement statement = null;
-		Connection connection = null;
-		ResultSet resultset = null;
-		log.info("Getting all public sample - biosample ids");
-		String query = "SELECT SAMPLE_ID,BIOSAMPLE_ID FROM SAMPLE WHERE STATUS_ID = 4 AND EGA_ID IS NULL " +
-				"AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?))";
+    public Map<String, String> getPublicSampleIds(Date minDate, Date maxDate) {
+        PreparedStatement statement = null;
+        Connection connection = null;
+        ResultSet resultset = null;
+        log.info("Getting all public sample - biosample ids");
+        String query = "SELECT SAMPLE_ID, BIOSAMPLE_ID FROM SAMPLE WHERE STATUS_ID = 4 AND EGA_ID IS NULL "
+                + "AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?))";
 
-		Map<String, String> sampleIds = new HashMap<String, String>();
+        Map<String, String> sampleIds = new HashMap<String, String>();
 
-		try {
-			BoneCPDataSource datasource = getDataSource();
-			connection = datasource.getConnection();
-			statement = connection.prepareStatement(query);
-			statement.setDate(1, new java.sql.Date(minDate.getTime()));
-			statement.setDate(2, new java.sql.Date(maxDate.getTime()));
-			statement.setDate(3, new java.sql.Date(minDate.getTime()));
-			statement.setDate(4, new java.sql.Date(maxDate.getTime()));
-			resultset = statement.executeQuery();
-			if (resultset == null) {
-				log.info("No Updates for sample mapping during the time period provided");
-			} else {
-				while (resultset.next()) {
-					String sampleId = resultset.getString(1); // result sets are one-indexed,
-					String biosampleId = resultset.getString(2);
-					sampleIds.put(sampleId, biosampleId);
-				}
-			}
-		} catch (SQLException e) {
-			log.error("Problem acessing database", e);
-		} catch (ClassNotFoundException e) {
-			log.error(
-					"The BoneCPDatasouce class for connection to the database cannot be found",
-					e);
-		} finally {
-			// close each of these separately in case of errors
-			if (resultset != null) {
-				try {
-					resultset.close();
-				} catch (SQLException e) {
-					// do nothing
-				}
-			}
-			if (statement != null) {
-				try {
-					statement.close();
-				} catch (SQLException e) {
-					// do nothing
-				}
-			}
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					// do nothing
-				}
-			}
-		}
+        try {
+            BoneCPDataSource datasource = getDataSource();
+            connection = datasource.getConnection();
+            statement = connection.prepareStatement(query);
+            statement.setDate(1, new java.sql.Date(minDate.getTime()));
+            statement.setDate(2, new java.sql.Date(maxDate.getTime()));
+            statement.setDate(3, new java.sql.Date(minDate.getTime()));
+            statement.setDate(4, new java.sql.Date(maxDate.getTime()));
+            resultset = statement.executeQuery();
+            if (resultset == null) {
+                log.info("No Updates for sample mapping during the time period provided");
+            } else {
+                while (resultset.next()) {
+                    String sampleId = resultset.getString(1); // result sets are one-indexed,
+                    String biosampleId = resultset.getString(2);
+                    sampleIds.put(sampleId, biosampleId);
+                }
+            }
+        } catch (SQLException e) {
+            log.error("Problem acessing database", e);
+        } catch (ClassNotFoundException e) {
+            log.error("The BoneCPDatasouce class for connection to the database cannot be found", e);
+        } finally {
+            // close each of these separately in case of errors
+            if (resultset != null) {
+                try {
+                    resultset.close();
+                } catch (SQLException e) {
+                    // do nothing
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    // do nothing
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    // do nothing
+                }
+            }
+        }
 
-		log.info("Got " + sampleIds.size() + " public sample ids");
+        log.info("Got " + sampleIds.size() + " public sample ids");
 
-		return sampleIds;
+        return sampleIds;
 
-	}
+    }
 
 }
--- src/main/java/uk/ac/ebi/fgpt/sampletab/tools/myeq/ERAsampleMapperDriver.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/tools/myeq/ERAsampleMapperDriver.java
@@ -3,75 +3,85 @@ package uk.ac.ebi.fgpt.sampletab.tools.myeq;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.ebi.fg.myequivalents.managers.interfaces.EntityMappingManager;
 import uk.ac.ebi.fg.myequivalents.resources.Resources;
-
+import uk.ac.ebi.fgpt.sampletab.AbstractDriver;
+import uk.ac.ebi.fgpt.sampletab.sra.EraProManager;
 
 /**
- * This class will collect all the 
- * mappings from the era pro database and update 
+ * This class will collect all the
+ * mappings from the era pro database and update
  * the myequivalents database
+ * 
  * @author drashtti
- *
+ * 
  */
 
-public class ERAsampleMapperDriver extends ERAsampleMapperCron{
-	
-	 @Argument(required = true, index = 1, metaVar = "STARTDATE", usage = "Start date as YYYY/MM/DD")
-	    protected String minDateString;
+public class ERAsampleMapperDriver extends AbstractDriver {
 
-	    @Argument(required = false, index = 2, metaVar = "ENDDATE", usage = "End date as YYYY/MM/DD")
-	    protected String maxDateString;
-	   
-	    private Logger log = LoggerFactory.getLogger(getClass());
-	    
-	    private EntityMappingManager emMgr;
+    @Argument(required = true, index = 1, metaVar = "STARTDATE", usage = "Start date as YYYY/MM/DD")
+    protected String minDateString;
 
-	    public static void main(String[] args) {
-	        new ERAsampleMapperDriver().doMain(args);
-	    }
-	    
-	    public void getSamples() {
-	        log.info("Getting sample ids from ERA-PRO");
-	        
-	        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
-	        Date minDate = null;
-	        Date maxDate = null;
-	        try {
-	            minDate = formatter.parse(minDateString);
-	            if (maxDateString != null) { 
-	                maxDate = formatter.parse(maxDateString);
-	            }
-	        } catch (ParseException e) {
-	            throw new RuntimeException(e);
-	        }
-	        
-	        Map<String, String> sampleIds = ERAsampleMapper.getInstance().getPublicSampleIds(minDate, maxDate);        
-	                
-	        log.info("Finishing getting sample ids from ERA-PRO"); 
-	        
-	        //call the method to store mappings in the myequivalents database
-	        storeMappings(sampleIds);
-	    }
+    @Argument(required = false, index = 2, metaVar = "ENDDATE", usage = "End date as YYYY/MM/DD")
+    protected String maxDateString;
 
-	    
-	    	public void storeMappings(Map<String,String> sampleIds){
-	    		log.info("connecting to the myequivalents database to store the mappings");
-	    		if(emMgr == null){
-	    		emMgr = Resources.getInstance ().getMyEqManagerFactory ().newEntityMappingManager();}
-	    		// Store a pair of entities that are linked together (i.e., are equivalent)
-	    		for (Map.Entry<String,String> entry : sampleIds.entrySet()){
-	    			String erasampleid = entry.getKey();
-	    			String biosampleid = entry.getValue();
-	    		emMgr.storeMappings ( 
-	    		  MyEqProperties.getENASamplesService() + ":" +erasampleid, MyEqProperties.getBioSamplesSamplesService() + ":"+biosampleid
-	    				);
-	    		}
-	    	}
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    private EntityMappingManager emMgr;
+
+    public static void main(String[] args) {
+        new ERAsampleMapperDriver().doMain(args);
+    }
+
+    @Override
+    public void doMain(String[] args) {
+        super.doMain(args);
+
+        //handle arguments into dates
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        Date minDate = null;
+        Date maxDate = null;
+        try {
+            minDate = formatter.parse(minDateString);
+            if (maxDateString != null) {
+                maxDate = formatter.parse(maxDateString);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        log.info("Getting sample ids from ERA-PRO");
+        
+        Map<String, String> sampleIds = EraProManager.getInstance().getSRABioSampleMappingForDateRange(minDate, maxDate);
+
+        log.info("Finishing getting sample ids from ERA-PRO");
+        
+        log.info("connecting to the myequivalents database to store the mappings");
+        if (emMgr == null) {
+            emMgr = Resources.getInstance().getMyEqManagerFactory().newEntityMappingManager();
+        }
+        // Store a pair of entities that are linked together (i.e., are equivalent)
+        for (Map.Entry<String, String> entry : sampleIds.entrySet()) {
+            String erasampleid = entry.getKey();
+            String biosampleid = entry.getValue();
+            log.info("Mapping "+biosampleid+" to "+erasampleid);
+            //ENA supports use of BioSample accessions, so do so
+            emMgr.storeMappings(MyEqProperties.getENASamplesService() + ":" + biosampleid,
+                    MyEqProperties.getBioSamplesSamplesService() + ":" + biosampleid);
+        }
+    }
+
 }
